a22f88fc191adae088587c509d92ef5dff931ade
unknown <dylan.castro@utec.edu.pe>
2018-09-12 18:15:23 -0500

    Final

--- vector.h
+++ vector.h
@@ -6,24 +6,32 @@ class Vector {
     public:
         typedef typename Tr::T T;
         typedef typename Tr::Operation Operation;
-             
     private:
         T* data;
         int dataSize;
-
         int dimensions;
         int* dimensionSizes;
-
+        Operation ope;
     public:
         Vector() : data(nullptr) {};
-             
         Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
             // TODO
+            dataSize=1;
+            for(int x=0;x<dimensions;x++){
+                dataSize*=dimensionSizes[x];
+            }
+            data = new T[dataSize];
         }
-             
-        void set(T datum, int* coordinates); // TODO
-             
-        T get(int* coordinates); // TODO
+        void set(T datum, int* coordinates){
+        // TODO
+            int position=ope(coordinates,dimensionSizes,dimensions);
+            data[position]=datum;
+        }
+        T get(int* coordinates){
+        // TODO
+            int position=ope(coordinates,dimensionSizes,dimensions);
+            return data[position];
+        }
 };
 
 #endif
